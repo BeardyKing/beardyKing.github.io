d1a4158bcedb8018a97bf7e5adf15acfa524f8a2
Dev <>
2018-08-03 00:39:53 +0100

    p5 initial push

---
+++ sketch.js
@@ -0,0 +1,12 @@
+
+
+function setup() {
+	createCanvas(windowWidth,400);
+	background(100,100,100);
+}
+
+function draw() {
+	fill(255,128,0,2);
+	noStroke();
+	r1 = rect(windowWidth/2,100,50,10);
+}
