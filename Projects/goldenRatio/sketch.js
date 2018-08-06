d8e6431067d4f6677f0cda2be39c14ef08f50199
Dev <>
2018-08-06 23:50:14 +0100

    added goldenRatio

---
+++ Projects/goldenRatio/sketch.js
@@ -0,0 +1,47 @@
+
+var w = 800;
+var h = 800;
+function setup() {
+	w = windowWidth
+	h = windowHeight;
+	createCanvas(w, h);
+	background(0);
+	//offset = 18;
+	offsetHolder = offset;
+}
+
+var i = 1;
+var j = 0;
+
+ var offset = 137.5;
+// var offset;
+var offsetHolder;
+
+function draw() {
+	//console.log(1 * 180/PI)
+	//background(50, 50,50);
+	fill(255, 128, 30, 200);
+	angleMode(DEGREES);
+	noStroke();
+
+	translate(w/2, h/2)
+	rotate(offset);
+	i += 0.1;
+	
+
+	for (let n = 0; n < 1; n++) {
+		offset += offsetHolder
+		ellipse(0	, i, 2, 2);
+			
+		}
+	
+}
+
+function keyPressed()
+{
+	if(keyCode === 49)
+	{
+		offsetHolder += 5;
+		console.log("added 5");
+	}
+}
